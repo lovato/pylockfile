f66a5f54a3e8ce5ba88bf08c7ac4c40bb536d5c4
Thiago Monaco Papageorgiou <thiago.monaco@corp.terra.com.br>
2014-01-28 17:49:14 -0200

    Reescrevendo link lock file

--- lockfile/linklockfile.py
+++ lockfile/linklockfile.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import
 import time
 import os
 import os.path
+import tempfile
+import md5
 
 from . import (LockBase, LockFailed, NotLocked, NotMyLock, LockTimeout,
                AlreadyLocked)
@@ -14,73 +16,77 @@ class LinkLockFile(LockBase):
     >>> lock = LinkLockFile('somefile', threaded=False)
     """
 
-    def acquire(self, timeout=None, expires_in=None):
-        if expires_in is not None:
-            if expires_in < 1:
-                expires_in = None
+    def __init__(self, path, threaded=False, timeout=None, expires_in=0):
+
+        tmp = tempfile.gettempdir()
+        path_hash = md5.new(path).hexdigest()
+        lock_file = os.path.join('%s/pylockfile/%s.lock' % (tmp, path_hash) )
+        self.expires_in = expires_in
+        
+        LockBase.__init__(self, path, threaded, timeout, lock_file)
+        
         try:
-            open(self.unique_name, "wb").close()
+            with open(self.unique_name, "wb") as file:
+                file.write("%d" % os.getpid())
         except IOError:
             raise LockFailed("failed to create %s" % self.unique_name)
 
+    def acquire(self, timeout=None, expires_in=None):
+
+        if expires_in is not None:
+            self.expires_in = expires_in
+        
         timeout = timeout is not None and timeout or self.timeout
         end_time = time.time()
         if timeout is not None and timeout > 0:
             end_time += timeout
 
         while True:
-            # Try and create a hard link to it.
+            
+            if not self.is_locked():
+                self.break_lock()
+
             try:
-                self.is_locked() #just for expiration check
-                try:
-                    os.link(self.unique_name, self.lock_file)
-                except:
-                    import shutil
-                    shutil.copyfile(self.unique_name, self.lock_file)
-                if expires_in is not None:
-                    f = open(self.lock_file,'w')
-                    f.write(str(expires_in))
-                    f.close()
+                os.link(self.unique_name, self.lock_file)
             except OSError:
-                # Link creation failed.  Maybe we've double-locked?
-                nlinks = os.stat(self.unique_name).st_nlink
-                if nlinks == 2:
-                    # The original link plus the one I created == 2.  We're
-                    # good to go.
-                    return
-                else:
-                    # Otherwise the lock creation failed.
-                    if timeout is not None and time.time() > end_time:
-                        os.unlink(self.unique_name)
-                        if timeout > 0:
-                            raise LockTimeout("Timeout waiting to acquire"
-                                              " lock for %s" %
-                                              self.path)
-                        else:
-                            raise AlreadyLocked("%s is already locked" %
-                                                self.path)
-                    time.sleep(timeout is not None and timeout/10 or 0.1)
+                if timeout is None or timeout <= 0:
+                    raise AlreadyLocked("%s is already locked" %
+                                        self.path)
+
+                if time.time() > end_time:
+                    raise LockTimeout("Timeout waiting to acquire"
+                                      " lock for %s" %
+                                      self.path)
+
+                time.sleep(1)
             else:
-                # Link creation succeeded.  We're good to go.
                 return
 
     def release(self):
+
+        # Essa operação tem situação de corrida
+
         if not self.is_locked():
-            raise NotLocked("%s is not locked" % self.path)
-        elif not os.path.exists(self.unique_name):
+            self.break_lock()
+            return
+        elif not self.i_am_locking():
             raise NotMyLock("%s is locked, but not by me" % self.path)
-        os.unlink(self.unique_name)
-        os.unlink(self.lock_file)
+        #os.unlink(self.unique_name)
+        self.break_lock()
 
     def is_locked(self):
         if self.is_lock_expired():
-            self.break_lock()
+            return False
         return os.path.exists(self.lock_file)
 
     def i_am_locking(self):
-        return (self.is_locked() and
-                os.path.exists(self.unique_name) and
-                os.stat(self.unique_name).st_nlink == 2)
+        if not self.is_locked():
+            return False
+
+        with open(self.lock_file, "r") as file:
+            lock_pid = file.read().strip()
+
+        return lock_pid == str(os.getpid()) 
 
     def break_lock(self):
         if os.path.exists(self.lock_file):
@@ -89,38 +95,22 @@ class LinkLockFile(LockBase):
     def get_lock_createtime(self):
         try:
             if os.path.exists(self.lock_file):
-                c_time = os.path.getctime(self.lock_file)
-                import datetime
-                return datetime.datetime.fromtimestamp(c_time)
-            else:
-                return None
+                stat = os.stat(self.lock_file)
+                return stat.st_ctime
         except:
-            return None
+            pass
+
+        return 0
          
     def get_lock_lifetime(self):
-        try:
-            lifetime = open(self.lock_file, 'r').read().strip()
-        except:
-            lifetime = ''
-        if len(lifetime) > 0:
-            if int(lifetime) > 0:
-                return int(lifetime)
-            else:
-                return -1
-        else:
-            return -1
+        return time.time() - self.get_lock_createtime()
     
     def is_lock_expired(self):
-        if self.get_lock_lifetime() < 0:
-            return False #lock will live forever
-        else:
-            try:
-                import datetime
-                max_valid_time = self.get_lock_createtime() + datetime.timedelta(seconds=self.get_lock_lifetime())
+        if self.expires_in <= 0:
+            return False
 
-                if datetime.datetime.now() > max_valid_time:
-                    return True
-                else:
-                    return False
-            except:
-                return True
+        create_time = self.get_lock_createtime()
+        if create_time == 0:
+            return True
+        else:
+            return self.get_lock_lifetime() > self.expires_in
